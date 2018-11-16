f124cac87ff0b7b93f1007c13acd5dea17c5a042
keshavmr <keshav@couchbase.com>
2018-11-15 18:53:06 -0800

    Create datasets and links on the ax service

---
+++ create_dataset.sh
@@ -0,0 +1,8 @@
+# create_indexes.sh
+. ./settings
+
+# Create datasets and link them.
+curl ${axqueryservice} -u Administrator:${pw} -XPOST -d "statement=create dataset on customers;"
+curl ${axqueryservice} -u Administrator:${pw} -XPOST -d "statement=create dataset on orders;"
+curl ${axqueryservice} -u Administrator:${pw} -XPOST -d "statement=connect link Local;"
+echo "$0 - Done"
